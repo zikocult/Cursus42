e4a79f07e10499da9c909a6a4e49f94413904bf3
zikocult <gbarulls@gmail.com>
2023-09-26 23:28:40 +0200

    Documentation

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+find $PWD -iname '*.out' -print 2>/dev/null -exec rm -f {} \;
+find $PWD -iname '*.swp' -print 2>/dev/null -exec rm -f {} \;
+find $PWD -iname '.DS_Store' -print 2>/dev/null -exec rm -f {} \;
+find $PWD -iname '*.gch' -print 2>/dev/null -exec rm -f {} \;
