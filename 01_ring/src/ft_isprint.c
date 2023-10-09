a94aea63b9771202ccfc1d5a5569699f376b8f86
Ziko Cult <gbarulls@gmail.com>
2023-10-09 23:26:27 +0200

    Libft.h

--- 01_ring/src/ft_isprint.c
+++ 01_ring/src/ft_isprint.c
@@ -3,30 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ZikoCult <ZikoCult>                        +#+  +:+       +#+        */
+/*   By: gbarulls <gbarulls@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/08 23:34:19 by ZikoCult          #+#    #+#             */
-/*   Updated: 2023/10/08 23:35:22 by ZikoCult         ###   ########.fr       */
+/*   Created: 2023/01/11 18:40:36 by gbarulls          #+#    #+#             */
+/*   Updated: 2023/10/09 23:25:38 by ZikoCult         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-int	ft_isascii(int c)
+int	ft_isprint(int c)
 {
-	if (c >= 32 && c <= 127)
+	if (c >= 32 && c < 127)
 		return (1);
 	return (0);
 }
 
 /* int main(void) */
 /* {  */
-/* printf("%i\n", ft_isascii('0')); */
-/* printf("%i\n", ft_isascii('1')); */
-/* printf("%i\n", ft_isascii('R')); */
-/* printf("%i\n", ft_isascii('/')); */
-/* printf("%i\n", ft_isascii(200)); */
-/* printf("%i\n", ft_isascii('\n')); */
-/* printf("%i\n", ft_isascii('9')); */
+/* printf("%i\n", ft_isprint('0')); */
+/* printf("%i\n", ft_isprint('1')); */
+/* printf("%i\n", ft_isprint('R')); */
+/* printf("%i\n", ft_isprint('/')); */
+/* printf("%i\n", ft_isprint(200)); */
+/* printf("%i\n", ft_isprint('\n')); */
+/* printf("%i\n", ft_isprint('9')); */
 /* return EXIT_SUCCESS; */
 /* } */
