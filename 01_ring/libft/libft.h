f20365a76598b46d5b07e32403f7a5baba198304
zikocult <gbarulls@gmail.com>
2023-10-13 18:08:40 +0200

    Libft.h

--- 01_ring/libft/libft.h
+++ 01_ring/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ZikoCult <ZikoCult>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 22:53:45 by ZikoCult          #+#    #+#             */
-/*   Updated: 2023/10/10 22:01:17 by ZikoCult         ###   ########.fr       */
+/*   Updated: 2023/10/13 18:08:23 by ZikoCult         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,8 @@ void	*ft_bzero(void *s, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
