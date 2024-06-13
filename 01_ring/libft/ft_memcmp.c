766ce602c5c0f0a3203e95b5e80d5338e23ade55
Guillem Baruls Casadesús <gbaruls-@student.42barcelona.com>
2024-06-13 17:01:53 +0200

    seguimos

--- 01_ring/libft/ft_memcmp.c
+++ 01_ring/libft/ft_memcmp.c
@@ -6,22 +6,33 @@
 /*   By: gbaruls- <gbaruls-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 13:14:28 by gbaruls-          #+#    #+#             */
-/*   Updated: 2024/06/13 13:21:45 by gbaruls-         ###   ########.fr       */
+/*   Updated: 2024/06/13 16:45:32 by gbaruls-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < n)
 	{
-		if(((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
-			return ((void *)(s1 + i) - (void *)(s2 + i));
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
-	return ((void *)(s1 + i) - (void *)(s2 + i));
+	return (0);
 }
+/*
+int main (int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("%i\n", ft_memcmp(argv[1], argv[2], 10));
+		printf("%i\n", memcmp(argv[1], argv[2], 10));
+	}
+	return (0);
+}*/
--- 01_ring/libft/libft.h
+++ 01_ring/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: gbaruls- <gbaruls-@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 11:43:43 by gbaruls-          #+#    #+#             */
-/*   Updated: 2024/06/13 13:14:10 by gbaruls-         ###   ########.fr       */
+/*   Updated: 2024/06/13 16:51:42 by gbaruls-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,5 +35,6 @@ char	*ft_strchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *s1, const char *s2, size_t len);
 
 #endif
