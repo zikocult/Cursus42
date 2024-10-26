75cf833d87055865fe551ea08b37580602f6ad16
Guillem Baruls Casadesús <gbaruls-@student.42barcelona.com>
2024-10-26 13:10:22 +0200

    Philosopher docs

---
+++ 04_ring/Philosophers/docus/print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 5;
+	float b = 5.543212;
+	char c[25] = "Hello World";
+	int d = 2;
+
+	printf("%s\nSuma: %i\nFloat dos decimales: %.2f\n", c, a + d, b);
+	return (0);
+}
---
+++ 04_ring/Philosophers/docus/usleep.c
@@ -0,0 +1,12 @@
+
+#include <unistd.h> 
+#include <stdio.h>
+  
+int main(void)
+{ 
+	printf("Primera linea\n");
+	usleep(3000000); 
+	printf("Segunda linea\n");
+	return (0); 
+}
+
